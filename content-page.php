8da0e804829554d13494c35b722bd7db09f2e15e
Patrick Kerby <patrick@kerbydesign.com>
2023-05-03 08:36:31 -0600

    events styling

--- content-page.php
+++ content-page.php
@@ -8,9 +8,9 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <header class="entry-header">
+    <!-- header class="entry-header">
         <h1 class="entry-title"><?php the_title(); ?></h1>
-    </header><!-- .entry-header -->
+    </header --><!-- .entry-header -->
 
     <div class="entry-content">
         <?php the_content(); ?>
--- custom_templates/events-home.php
+++ custom_templates/events-home.php
@@ -15,7 +15,7 @@ get_header();
  ?>
 
     <!-- PAGE START  -->
-    <div class="container-fluid heading">
+    <div class="container-fluid heading">        
         <div class="row justify-content-center" style="background-image: linear-gradient(174deg, rgba(27,48,46,0.62) 0%, rgba(43,77,74,0.62) 100%), url('<?php echo $heading_image['url'] ?>'); background-size: cover;">
             <h1><?php echo $title; ?></h1>
 
@@ -24,19 +24,20 @@ get_header();
                     <h2><?php echo $subtitle; ?></h2>
                 </div>
             <?php } ?>
-        </div>
+        </div>        
         <div class="row weeklyevents">
           <?php 
             $rows = get_field('weekly_events', 'option');
             if( $rows ) {
-              foreach( $rows as $row ) {
-                $title = $row['title'];
-                $desc = $row['weekly_event_description'];
-                echo '<div class="col-sm-4">';
+                echo '<h2>Weekly Events</h2>';
+                foreach( $rows as $row ) {
+                    $title = $row['title'];
+                    $desc = $row['weekly_event_description'];
+                    echo '<div class="col-sm-4">';
                     echo '<h3>'.$title.'</h3>';
                     echo '<p>'.$desc.'</p>';
-                echo '</div>';
-              }
+                    echo '</div>';
+                }
             }
           ?>
         </div>
@@ -45,8 +46,6 @@ get_header();
 
 
 <main id="tribe-events-pg-template" class="tribe-events-pg-template">
-
-
 
 <div id="primary" class="content-area col-md-12">
     <div id="content" class="site-content" role="main">
